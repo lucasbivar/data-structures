7a3ce05745dd038bfbdbe8c40f9077f4cf3bbe25
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-14 15:43:52 -0300

    add create function

---
+++ ADT/point.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <math.h>
+#include "point.h"
+
+struct point{
+  float x;
+  float y;
+};
+
+Point* create(float x, float y){
+  Point* p = (Point *)malloc(sizeof(Point));
+  if(p != NULL){
+    p->x = x;
+    p->y = y;
+  }
+  return p;
+}
