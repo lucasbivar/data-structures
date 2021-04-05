f7e0ff506eff36136726642df0961a7d62dec216
lucasbivar <lucasbivarfonseca@gmail.com>
2021-04-05 20:44:30 -0300

    implemented some functions with descriptor node

---
+++ DynamicListWithDescriptorNode/dynamicListWithDescriptorNode.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dynamicListWithDescriptorNode.h"
+
+struct node{
+  struct student data;
+  struct node* next;
+};
+
+typedef struct node Node;
+
+struct descriptor{
+  struct node* head;
+  struct node* tail;
+  int size;
+};
+
+List* create(){
+  List* li = (List*) malloc(sizeof(List));
+  if(li != NULL){
+    li->head = NULL;
+    li->tail = NULL;
+    li->size = 0;
+  }
+  return li;
+}
+
+void release(List* list){
+  if(list != NULL){
+    Node* n;
+    while((list->head) != NULL){
+      n = list->head;
+      list->head = list->head->next;
+      free(n);
+    }
+    free(list);
+  }
+}
+
+int size(List* list){
+  if(list == NULL) return 0;
+  return list->size;
+}
+
+int pushfront(List* list, struct student student){
+  if(list == NULL) return 0;
+
+  Node* n = (Node*) malloc(sizeof(Node));
+  if(n == NULL) return 0;
+
+  n->data = student;
+  n->next = list->head;
+
+  if(list->head == NULL){
+    list->tail = n;
+  }
+
+  list->head = n;
+  list->size++;
+  return 1;
+}
+
+int pushback(List* list, struct student student){
+  if(list == NULL) return 0;
+
+  Node* n = (Node*) malloc(sizeof(Node));
+  if(n == NULL) return 0;
+
+  n->data = student;
+  n->next = NULL;
+
+  if(list->head == NULL){
+    list->head = n;
+  }else{
+    list->tail->next = n;
+  }
+
+  list->tail = n;
+  list->size++;
+  return 1;
+}
+
+int removefront(List* list){
+  if(list == NULL) return 0;
+
+  if(list->head == NULL) return 0;
+
+  Node* n = list->head;
+  list->head = n->next;
+  free(n);
+
+  if(list->head == NULL){
+    list->tail = NULL;
+  }
+  list->size--;
+  return 1;
+}
+
+int removeback(List* list){
+  if(list == NULL) return 0;
+
+  if(list->head == NULL) return 0;
+
+  Node *prev, *n = list->head;
+  while(n->next != NULL){
+    prev = n;
+    n = n->next;
+  }
+
+  if(n == list->head){
+    list->head = NULL;
+    list->tail = NULL;
+  }else{
+    prev->next = n->next;
+    list->tail = prev;
+  }
+
+  free(n);
+  list->size--;
+  return 1;
+}
---
+++ DynamicListWithDescriptorNode/dynamicListWithDescriptorNode.h
@@ -0,0 +1,22 @@
+struct student{
+  int enrollment;
+  char name[20];
+  float p1, p2, p3; 
+};
+
+typedef struct descriptor List;
+
+List* create();
+
+void release(List* list);
+
+int size(List* list);
+
+int pushfront(List* list, struct student student);
+
+int pushback(List* list, struct student student);
+
+int removefront(List* list);
+
+int removeback(List* list);
+
