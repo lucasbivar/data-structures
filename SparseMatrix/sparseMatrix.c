c6d6e38b6bbcdb67a143d0c1826bcabf5b1924e5
lucasbivar <lucasbivarfonseca@gmail.com>
2021-04-14 20:04:53 -0300

    implemented assign and get functions

--- SparseMatrix/sparseMatrix.c
+++ SparseMatrix/sparseMatrix.c
@@ -34,3 +34,64 @@ sparseMatrix* create(int rows, int columns){
   }
   return ptr;
 }
+
+int assign(sparseMatrix* matrix, int row, int column, float data){
+  if(row < 0 || row >= matrix->rows 
+  || column < 0 || column >= matrix->columns) return 0;
+
+  PONT previous = NULL, current = matrix->arrangement[row];
+  while(current != NULL && current->column < column){
+    previous = current;
+    current = current->next;
+  }
+
+  // Node exists?
+  if(current != NULL && current->column == column){
+    if(data == 0){
+      //First element?
+      if(previous == NULL){
+        matrix->arrangement[row] = current->next;
+      }else{
+        previous->next = current->next;
+      }
+      free(current);
+    }else{
+      current->data = data;
+    }
+  }else{
+    //We need storage the node?
+    if(data != 0){
+      PONT newNode = (PONT) malloc(sizeof(Node));
+      if(newNode == NULL) return 0;
+
+      newNode->column = column;
+      newNode->data = data;
+      newNode->next = current;
+
+      //Will the node be in the first position or the row is empty or ?
+      if(previous == NULL){
+        matrix->arrangement[row] = newNode;
+      }else{
+        previous->next = newNode;
+      }
+    }
+  }
+  return 1;
+}
+
+int getData(sparseMatrix* matrix, int row, int column, float* data){
+  if(row < 0 || row >= matrix->rows 
+  || column < 0 || column >= matrix->columns) return 0;
+
+  PONT current = matrix->arrangement[row];
+  while(current != NULL && current->column < column){
+    current = current->next;
+  }
+  
+  if(current != NULL && current->column == column){
+    *data = current->data;
+  }else{
+    *data = 0;
+  }
+  return 1;
+}
--- SparseMatrix/sparseMatrix.h
+++ SparseMatrix/sparseMatrix.h
@@ -1,3 +1,7 @@
 typedef struct matrix sparseMatrix;
 
 sparseMatrix* create(int rows, int columns);
+
+int assign(sparseMatrix* matrix, int row, int column, float data);
+
+int getData(sparseMatrix* matrix, int row, int column, float* data);
