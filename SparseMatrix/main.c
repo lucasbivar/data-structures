f6a36946947ac256777fb20f0a354f81de847cbe
lucasbivar <lucasbivarfonseca@gmail.com>
2021-04-14 22:13:48 -0300

    implemented some functions and finished menu

--- SparseMatrix/main.c
+++ SparseMatrix/main.c
@@ -1,6 +1,112 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "sparseMatrix.h"
+
+void show_options(int isQuadratic){
+  printf("\n=-=-=-=-=-=-=-=-=-Sparse Matrix-=-=-=-=-=-=-=-=-=\n");
+  printf("[1] Assign\n");
+  printf("[2] Get\n");
+  printf("[3] Show\n");
+  if(isQuadratic){
+    printf("[4] Primary Diagonal\n");
+    printf("[5] Secondary Diagonal\n");
+  }
+  printf("[0] EXIT\n");
+}
+
+void clear_buffer(){
+  int ch;
+  while((ch = getchar()) != EOF && ch != '\n' );
+}
 
 int main(void){
+  sparseMatrix* matrix = NULL;
+  int option;
+  int loop = 1;
+  int row, column, data, max_option = 5;
+  printf("Let's build the Sparse Matrix\n");
+  printf("Insert the row size of matrix:\n");
+  scanf("%d", &row);
+  clear_buffer();
+  printf("Insert the column size of matrix:\n");
+  scanf("%d", &column);
+  clear_buffer();
+  printf("\n");
+  int isQuadratic = 1;
+  if(row != column){
+    isQuadratic = 0;
+    max_option = 3;
+  }
+  matrix = create(row, column);
+  if(matrix != NULL){
+    show(matrix);
+  }else{
+    printf("Error creating matrix\n");
+    return 0;
+  }
+  while(loop){
+    show_options(isQuadratic);
+    while(1){
+      printf("\nType: ");
+      scanf("%d", &option);
+      clear_buffer();
+      if (option >= 0 && option <= max_option) break; 
+      else{
+        printf("\nError: Invalid Option\n");
+      }
+    }
+    switch (option){
+      case 0:
+        release(matrix);
+        loop = 0;
+        break;
+      case 1:
+        printf("Insert the row:\n");
+        scanf("%d", &row);
+        clear_buffer();
+        printf("Insert the column:\n");
+        scanf("%d", &column);
+        clear_buffer();
+        printf("Insert the value (int):\n");
+        scanf("%d", &data);
+        clear_buffer();
+        if(assign(matrix,row, column, data)){
+          printf("The data [%d] was assigned at [%d][%d].", data, row, column);
+        }else{
+          printf("Invalid postion");
+        }
+        break;
+      case 2:
+        printf("Insert the row:\n");
+        scanf("%d", &row);
+        clear_buffer();
+        printf("Insert the column:\n");
+        scanf("%d", &column);
+        clear_buffer();
+        if(getData(matrix,row, column, &data)){
+          printf("The element at the position [%d][%d] is %d!\n", row, column, data);
+        }else{
+          printf("Invalid postion");
+        }
+        break;
+      case 3:
+        if(!show(matrix)){
+          printf("Error\n\n");
+        }
+        break;
+      case 4:
+        if(!getPrimaryDiagonal(matrix)){
+          printf("Error\n");
+        }
+        break;
+      case 5:
+        if(!getSecondaryDiagonal(matrix)){
+          printf("Error\n");
+        }
+        break;
+      default:
+        break;
+    }
+  }
   return 0;
 }
--- SparseMatrix/sparseMatrix.c
+++ SparseMatrix/sparseMatrix.c
@@ -3,7 +3,7 @@
 #include "sparseMatrix.h"
 
 struct node{
-  float data;
+  int data;
   int column;
   struct node* next;
 };
@@ -35,7 +35,7 @@ sparseMatrix* create(int rows, int columns){
   return ptr;
 }
 
-int assign(sparseMatrix* matrix, int row, int column, float data){
+int assign(sparseMatrix* matrix, int row, int column, int data){
   if(row < 0 || row >= matrix->rows 
   || column < 0 || column >= matrix->columns) return 0;
 
@@ -79,7 +79,7 @@ int assign(sparseMatrix* matrix, int row, int column, float data){
   return 1;
 }
 
-int getData(sparseMatrix* matrix, int row, int column, float* data){
+int getData(sparseMatrix* matrix, int row, int column, int* data){
   if(row < 0 || row >= matrix->rows 
   || column < 0 || column >= matrix->columns) return 0;
 
@@ -93,5 +93,91 @@ int getData(sparseMatrix* matrix, int row, int column, float* data){
   }else{
     *data = 0;
   }
+  return 1;
+}
+
+void release(sparseMatrix* matrix){
+  if(matrix != NULL){
+    PONT current, temp;
+    for(int i = 0; i < matrix->rows; i++){
+      current = matrix->arrangement[i];
+      while(current != NULL){
+        temp = current;
+        current = current->next;
+        free(temp);
+      }
+    }
+    free(matrix->arrangement);
+    free(matrix);
+  }
+}
+
+int show(sparseMatrix* matrix){
+  if(matrix == NULL) return 0;
+  PONT aux;
+  printf("\n=-=-=-=-=-=-=-=-=-Sparse Matrix-=-=-=-=-=-=-=-=-=\n");
+  for(int i = 0; i < matrix->rows;i++){
+    aux = matrix->arrangement[i];
+    for(int j = 0; j < matrix->columns; j++){
+      if(aux != NULL && aux->column == j){
+        printf("%d\t", aux->data);
+        aux = aux->next;
+      }else{
+        printf("0\t");
+      }
+    }
+    printf("\n");
+  }
+  return 1;
+}
+
+int getPrimaryDiagonal(sparseMatrix* matrix){
+  if(matrix == NULL) return 0;
+
+  printf("The Primary Diagonal is: [");
+  PONT aux;
+  int value = 0;
+  for(int i = 0; i < matrix->rows; i++){
+    value = 0;
+    aux = matrix->arrangement[i];
+    while(aux != NULL && aux->column <= i){
+      if(aux->column == i){
+        value = aux->data;
+        break;
+      }
+      aux = aux->next;
+    }
+    if(i == matrix->rows-1){
+      printf("%d",value);
+    }else{
+      printf("%d\t",value);
+    }
+  }
+  printf("]\n");
+  return 1;
+}
+
+int getSecondaryDiagonal(sparseMatrix* matrix){
+  if(matrix == NULL) return 0;
+   printf("The Secondary Diagonal is: [");
+  PONT aux;
+  int value = 0;
+  for(int i = 0; i < matrix->rows; i++){
+    value = 0;
+    aux = matrix->arrangement[i];
+    while(aux != NULL && ((i + aux->column) <= matrix->columns-1)){
+      if(aux->column + i == matrix->columns-1){
+        value = aux->data;
+        break;
+      }
+      aux = aux->next;
+    }
+    if(i == matrix->rows-1){
+      printf("%d",value);
+    }else{
+      printf("%d\t",value);
+    }
+  }
+  printf("]\n");
   return 1;
 }
