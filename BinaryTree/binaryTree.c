b781220ffd83889c409ec8cab873c6f3d2cf7a3a
lucasbivar <lucasbivarfonseca@gmail.com>
2021-05-11 19:14:32 -0300

    adding create and release functions

--- BinaryTree/binaryTree.c
+++ BinaryTree/binaryTree.c
@@ -7,3 +7,25 @@ struct NODE{
   struct NODE* left;
   struct NODE* right;
 };
+
+binaryTree* create(){
+  binaryTree* root = (binaryTree*) malloc(sizeof(binaryTree));
+  if(root != NULL){
+    *root = NULL;
+  }
+  return root;
+}
+
+void releaseNode(struct NODE* node){
+  if(node == NULL) return;
+  releaseNode(node->left);
+  releaseNode(node->right);
+  free(node);
+  node = NULL;
+}
+
+void releaseTree(binaryTree* root){
+  if(root == NULL) return;
+  releaseNode(*root);
+  free(root);
+}
--- BinaryTree/binaryTree.h
+++ BinaryTree/binaryTree.h
@@ -1,1 +1,7 @@
 typedef struct NODE* binaryTree;
+
+binaryTree* create();
+
+void releaseNode(struct NODE* node);
+
+void releaseTree(binaryTree* root);
